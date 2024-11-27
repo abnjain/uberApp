25e7745351e5d3fcfea77ee3a7ec4dafaa87c681
Janmejay <abnjain25@gmail.com>
2024-11-27 15:59:32 +0530

    started working on the project and created a server

---
+++ backend/app.js
@@ -0,0 +1,9 @@
+const dotenv = require("dotenv");
+dotenv.config();
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+app.use(cors());
+
+module.exports = app;
